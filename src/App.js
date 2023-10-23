b93f1808ec881a0f779801cdf177a9f7ff4160b3
Alina <logunionok@gmail.com>
2023-10-23 23:09:44 +0300

    feat: create app template, create matrix cell component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import { Cell } from "./components";
 
 function App() {
+  const length = 10;
+  const matrix = Array.from({ length: length }, () =>
+    Array.from({ length: length }, () => 1)
+  );
   return (
     <div className="App">
-    
+      {matrix.map((matrixLine, lineIndex) => {
+        return (
+          <div className="line-container">
+            {matrixLine.map((element, columnIndex) => (
+              <Cell key={`${lineIndex}-${columnIndex}`} value={element} />
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Cell from "./Cell/Cell";
+
+export { Cell };
