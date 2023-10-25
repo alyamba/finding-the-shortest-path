43a00434c341436f5abb14ba42558ee6c011578e
Alina <logunionok@gmail.com>
2023-10-26 01:07:45 +0300

    Updates

--- src/components/Modal/Modal.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Modal.css";
-
-const Modal = ({ message }) => {
-  return <div className="status">{message}</div>;
-};
-
-export default Modal;
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Cell from "./Cell/Cell";
-import Button from "./Button/Button";
-import Matrix from "./Matrix/Matrix";
-import Modal from "./Modal/Modal";
-
-export { Cell, Button, Matrix, Modal };
